af0eba1fec62adbba52b528ed2fcf585a7556915
70 - Preeti Swati <kswati1398@gmail.com>
2021-03-22 05:18:43 +0530

    local file upload support

--- backend/routes/docTransfer.js
+++ backend/routes/docTransfer.js
@@ -1,17 +1,32 @@
 const express = require("express");
 let router = express.Router();
+var multer  = require('multer');
+var upload = multer();
+
 const { docTransfer } = require('../services/DocTransferUtility');
+
 
 router.use(express.json());
 
 //route for transfer of file
-router.post("/",(req,res) => {
-    const { sourceConfig , destinationConfig }= req.body;
-    console.log(req.body);
+router.post("/", upload.any(), (req,res) => {
 
-    docTransfer(sourceConfig,destinationConfig)
-    .then(result =>  res.status(200).json({result}))
-    .catch(err => res.status(500).json({err}));
+    // file transfer from form data file
+    if(req.files.length>0){
+        const { sourceConfig , destinationConfig }= JSON.parse(req.body.reqJson);
+        const files = req.files;
+
+        docTransfer(sourceConfig,destinationConfig,files)
+        .then(result =>  res.status(200).json({result}))
+        .catch(err => res.status(500).json({err}));
+    // file transfer from other external sources
+    }else{
+        const { sourceConfig , destinationConfig }= req.body;
+
+        docTransfer(sourceConfig,destinationConfig)
+        .then(result =>  res.status(200).json({result}))
+        .catch(err => res.status(500).json({err}));
+    }
     
 });
 
--- backend/services/DocTransferUtility.js
+++ backend/services/DocTransferUtility.js
@@ -1,18 +1,24 @@
 const functionLookup = {
     "source" : {
-        "GoogleDrive" : "./data_sources/GoogleDrive"
+        "GoogleDrive" : "./data_sources/GoogleDrive",
+        "LocalStorage" : "./data_sources/LocalFile",
+        "DigiMocker": "./data_sources/DigiMocker"
     },
     "destination" : {
         "AwsS3" : "./data_destinations/AwsS3"
     }
 }
 
-async function docTransfer(sourceConfig, destinationConfig){
+async function docTransfer(sourceConfig, destinationConfig, files){
     const source = sourceConfig.name;
     const storage = destinationConfig.name;
-
     const {downloadFile} = require(functionLookup["source"][source]);
     const {uploadFile} = require(functionLookup["destination"][storage]);
+
+    // file provided directly
+    if(files){
+        sourceConfig.extendedData = files
+    }
 
     return new Promise((resolve,reject) => {
         downloadFile(sourceConfig.extendedData)
@@ -23,6 +29,7 @@ async function docTransfer(sourceConfig, destinationConfig){
             .catch(error => reject(error));
         })
         .catch(error => {
+            console.log(error);
             reject(error);
         });
     });
